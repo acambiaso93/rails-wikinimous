d71577354cc5cd65461a9dcd6f74ccdb762e261a
Alexandra Cambiaso <acambiaso93@gmail.com>
2024-01-22 20:32:24 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require  'faker'
+
+20.times do
+  Article.create(title: Faker::Book.title, content: Faker::Quotes::Shakespeare.hamlet_quote)
+end
